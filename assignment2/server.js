bafd8b040d4987f7979baf88e1a6a7f4d051f373
Mudit <muditsing704@gmail.com>
2026-09-15 00:18:45 +0530

    assignment_work

---
+++ assignment2/server.js
@@ -0,0 +1,50 @@
+
+import express from "express";
+
+const app = express();
+
+const PORT = 4444;
+
+app.use(express.json());
+
+// 100 Products
+const products = [];
+
+for (let i = 1; i <= 100; i++) {
+    products.push({
+        id: i,
+        name: `Product ${i}`,
+        price: i * 100,
+        category: `Category ${((i - 1) % 10) + 1}`,
+        inStock: true
+    });
+}
+
+// Home
+app.get("/", (req, res) => {
+    res.send("Product REST API is running");
+});
+
+// Get all products
+app.get("/products", (req, res) => {
+    res.json(products);
+});
+
+// Get product by ID
+app.get("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const product = products.find(p => p.id === id);
+
+    if (!product) {
+        return res.status(404).json({
+            message: "Product not found"
+        });
+    }
+
+    res.json(product);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
